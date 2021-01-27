faeee6e23145b6dfce07cbe83b5b6255a477b90b
Nick2020 <math67can6448902005@gmail.com>
2021-01-27 09:32:02 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+  
+const RPC = require('discord-rpc');
+const client = new RPC.Client({ transport: 'ipc' });
+client.on('ready', () =>
+{
+    client.request('SET_ACTIVITY', {
+        pid: process.pid,
+        activity: {
+            assets: {
+                large_image: 'woc'
+            },
+            details: '3.6K CODING SERVER!',
+            buttons: [
+                { label: 'Join Me!', url: 'https://discord.gg/XTsCQNd' }
+            ]
+        }
+    });
+    console.log('started!');
+});
+client.login({ clientId: '803978215466074122' });
